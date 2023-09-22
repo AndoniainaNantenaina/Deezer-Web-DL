9c39fb41d6fff02203bbb95a54b8b3f1fd7eca7d
Andoniaina516 <ando.nomenjanahary@futurmap.com>
2023-09-22 08:38:31 +0300

    Removing .firebase/ folder in frontend.

--- frontend/src/pages/about.tsx
+++
@@ -1,7 +0,0 @@
-export default function About() {
-    return(
-        <div className="h-screen bg-slate-900 text-white p-2">
-            <h1>About</h1>
-        </div>
-    );
-}
--- frontend/src/pages/donate.tsx
+++
@@ -1,7 +0,0 @@
-export default function Donate() {
-    return(
-        <div className="h-screen bg-slate-900 text-white p-2">
-            <h1>Donate !</h1>
-        </div>
-    );
-}
